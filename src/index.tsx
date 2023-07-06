5534c8fb7088d24f173aadacc9b61d4e1930541f
godfreykaris <godfreykarisw@gmail.com>
2023-07-06 20:45:51 +0300

    Web3 + React + Typescript

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) 
+{
+    const root = createRoot(rootElement);
+
+    root.render
+    (
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+    )
+}
