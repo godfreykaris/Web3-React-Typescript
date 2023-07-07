cf69af395d74b495785d8534c094d0ad594ddec9
godfreykaris <godfreykarisw@gmail.com>
2023-07-07 17:11:11 +0300

    Web3 + React + Typscript + Metamask

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { ChakraProvider } from "@chakra-ui/react";
-import ConnectButton from "./components/ConnectButton";
+import { ChakraProvider, useDisclosure } from "@chakra-ui/react";
 import Layout from "./components/Layout";
+import theme from "./theme";
+import ConnectButton from "./components/ConnectButton";
+import AccountModal from "./components/AccountModal";
+import "@fontsource/inter";
 
 export default function App() {
+  
+  // Pull the disclosure methods
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <ChakraProvider>
+    
+    <ChakraProvider theme={theme}>
       <Layout>
-        <ConnectButton />
+        {/* Our connect button will only handle opening */}
+        <ConnectButton handleOpenModal={onOpen} />
+        {/* Our Account modal will handle open state & closing */}
+        <AccountModal isOpen={isOpen} onClose={onClose} />
       </Layout>
     </ChakraProvider>
   )
---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+const theme = {
+    primaryColor: "#FF0000",
+    secondaryColor: "#00FF00",
+    // other theme properties
+  };
+  
+  export default theme;
+  
